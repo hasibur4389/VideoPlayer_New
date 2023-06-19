b3158c9bb4dc4c6aa79faa9d4017f36c0fffd244
hasibur4389 <hasibur@braincraftapps.com>
2023-06-19 19:29:22 +0600

    Initial Commit

---
+++ VideoPlayer_New/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayer_New
+//
+//  Created by Hasibur Rahman on 19/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
